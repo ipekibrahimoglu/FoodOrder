2537082e880085592d64cb2c774f60a8af5b00d4
Muratcan Kara <muratfivem@gmail.com>
2025-04-12 16:07:31 +0300

    DI container added

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,4 +1,10 @@
 
+using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
+using Microsoft.EntityFrameworkCore;
+
 namespace WebAPI
 {
     public class Program
@@ -7,12 +13,48 @@ namespace WebAPI
         {
             var builder = WebApplication.CreateBuilder(args);
 
+            builder.Services.AddDbContext<SouthwindContext>(options => options.UseSqlServer(
+                builder.Configuration.GetConnectionString("DefaultConnection")));
             // Add services to the container.
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+            //User
+            builder.Services.AddScoped<IUserService, UserManager>();
+            builder.Services.AddScoped<IUserDal, EfUserDal>();
+
+            //Menu
+            builder.Services.AddScoped<IMenuService, MenuManager>();
+            builder.Services.AddScoped<IMenuDal, EfMenuDal>();
+
+            //MenuItem
+            builder.Services.AddScoped<IMenuItemService, MenuItemManager>();
+            builder.Services.AddScoped<IMenuItemDal, EfMenuItemDal>();
+
+            //Order
+            builder.Services.AddScoped<IOrderService, OrderManager>();
+            builder.Services.AddScoped<IOrderDal, EfOrderDal>();
+
+            //OrderItem
+            builder.Services.AddScoped<IOrderItemService, OrderItemManager>();
+            builder.Services.AddScoped<IOrderItemDal, EfOrderItemDal>();
+
+            //Payment
+            builder.Services.AddScoped<IPaymentService, PaymentManager>();
+            builder.Services.AddScoped<IPaymentDal, EfPaymentDal>();
+
+            //Restaurant
+            builder.Services.AddScoped<IRestaurantService, RestaurantManager>();
+            builder.Services.AddScoped<IRestaurantDal, EfRestaurantDal>();
+
+            //Review
+            builder.Services.AddScoped<IReviewService, ReviewManager>();
+            builder.Services.AddScoped<IReviewDal, EfReviewDal>();
+
+            
 
             var app = builder.Build();
 
