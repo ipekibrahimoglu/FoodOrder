526f4f96b45c8578336ccfe2612824972e33e5bb
Muratcan Kara <muratfivem@gmail.com>
2025-04-19 14:52:21 +0300

    FluentValidation has been integrated inside of the WebAPI. Furthermore, It has been added into the Core Layer just in case.

---
+++ Core/CrossCuttingConcerns/Validation/FluentValidation/FluentValidationTool.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+using FluentValidation.Results;
+
+namespace Core.CrossCuttingConcerns.Validation.FluentValidation
+{
+    public class FluentValidationTool : IValidatorTool
+    {
+        public void Validate<T>(IValidator<T> validator, T entity)
+        {
+            ValidationResult result = validator.Validate(entity);
+            if (!result.IsValid)
+            {
+                throw new ValidationException(result.Errors);
+            }
+        }
+    }
+}
---
+++ Core/CrossCuttingConcerns/Validation/FluentValidation/IValidatorTool.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+
+namespace Core.CrossCuttingConcerns.Validation.FluentValidation
+{
+    public interface IValidatorTool
+    {
+        void Validate<T>(IValidator<T> validator, T entity);
+    }
+}
